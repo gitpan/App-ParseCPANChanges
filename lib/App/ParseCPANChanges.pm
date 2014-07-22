fe70b0da261f40942cb94c94b8162b589e94549b
Steven Haryanto <stevenharyanto@gmail.com>
2014-07-22 16:40:03 +0000

    Import of SHARYANTO/App-ParseCPANChanges-0.04 from CPAN.
    gitpan-cpan-distribution: App-ParseCPANChanges
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SHARYANTO/App-ParseCPANChanges-0.04.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/ParseCPANChanges.pm
+++ lib/App/ParseCPANChanges.pm
@@ -4,7 +4,7 @@ use 5.010001;
 use strict;
 use warnings;
 
-our $VERSION = '0.03'; # VERSION
+our $VERSION = '0.04'; # VERSION
 
 our %SPEC;
 
@@ -32,6 +32,9 @@ sub parse_cpan_changes {
 	    do { $file = $_; last } if -f $_;
 	}
     }
+    return [400, "Please specify file ".
+                "(or run in directory where Changes file exists)"]
+        unless $file;
 
     my $ch = CPAN::Changes->load($file);
     [200, "OK", Data::Structure::Util::unbless($ch)];
@@ -44,7 +47,7 @@ __END__
 
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
@@ -52,7 +55,7 @@ App::ParseCPANChanges - Parse CPAN Changes file
 
 =head1 VERSION
 
-version 0.03
+This document describes version 0.04 of App::ParseCPANChanges (from Perl distribution App-ParseCPANChanges), released on 2014-07-22.
 
 =head1 DESCRIPTION
 
@@ -78,7 +81,16 @@ If not specified, will look for file called Changes/ChangeLog in current directo
 
 Return value:
 
-Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
+Returns an enveloped result (an array).
+
+First element (status) is an integer containing HTTP status code
+(200 means OK, 4xx caller error, 5xx function error). Second element
+(msg) is a string containing error message, or 'OK' if status is
+200. Third element (result) is optional, the actual result. Fourth
+element (meta) is called result metadata and is optional, a hash
+that contains extra information.
+
+ (any)
 
 =head1 SEE ALSO
 
@@ -103,8 +115,7 @@ Source repository is at L<https://github.com/sharyanto/perl-App-ParseCPANChanges
 
 =head1 BUGS
 
-Please report any bugs or feature requests on the bugtracker website
-http://rt.cpan.org/Public/Dist/Display.html?Name=App-ParseCPANChanges
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-ParseCPANChanges>
 
 When submitting a bug or request, please include a test-file or a
 patch to an existing test-file that illustrates the bug or desired
@@ -116,7 +127,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
